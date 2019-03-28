19cce55b70229d629d58255517762a23c6e04dcd
td94 <quynhtrangdo94@gmail.com>
2019-03-28 11:56:45 -0700

    Check for valid move

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 const promt = require('prompt');
+
+const TicTacToe = function() {
+  this.board = [[1,2,3],[4,5,6],[7,8,9]];
+  this.currentPlayer = 'x';
+  this.moves = 0
+}
+
+TicTacToe.prototype.switchPlayer = function() {
+  this.currentPlayer = this.currentPlayer === 'x' ? 'o' : 'x';
+}
+
+TicTacToe.prototype.checkValidMove = function(move) {
+  move = Number(move);
+  if(move > 9 || move < 1) {
+    console.log('number must be between 1 and 9');
+    return false;
+  } else if (Math.floor(move) !== move) {
+    console.log('number must be an integer');
+    return false;
+  } else if(this.isPositionOccupied(move)) {
+    console.log('that position is already occupied');
+    return false;
+  } else return true;
+}
+
+var game = new TicTacToe();
+game.play();
